328c47e8ba71e972f3d596580468034ed9301ac8
oakarkyaw <oakarkyaw7090@gmail.com>
2026-08-18 17:19:38 +0630

    feat(main): initial set up

--- set-up.sh
+++ set-up.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 set -e
 
-echo "Install docker"
+echo "Install Docker"
 
-# Add Docker's official GPG key:
 sudo apt update
-sudo apt install ca-certificates curl
+
+sudo apt install -y ca-certificates curl
+
+# Add Docker's official GPG key
 sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+
+sudo curl -fsSL \
+  https://download.docker.com/linux/ubuntu/gpg \
+  -o /etc/apt/keyrings/docker.asc
+
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add Docker repository
 sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/ubuntu
@@ -22,15 +28,20 @@ EOF
 
 sudo apt update
 
-echo "install docker"
+echo "Installing Docker..."
 
-sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt install -y \
+  docker-ce \
+  docker-ce-cli \
+  containerd.io \
+  docker-buildx-plugin \
+  docker-compose-plugin
 
-echo "docker starting..."
+echo "Docker starting..."
 
-sudo systemctl status docker
+sudo systemctl enable --now docker
+
+echo "Docker status:"
+sudo systemctl --no-pager status docker
 
 echo "End successfully 💃🏿"
-
-
-
